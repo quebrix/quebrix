4cc267ccc5bb37dba9722f9ea0152fb6e28d066a
alireza.baloochi1380@gmail.com <alireza.baloochi1380@gmail.com>
2024-09-04 14:53:36 +0330

    Rename branding

--- build.rs
+++ build.rs
@@ -3,63 +3,60 @@ use std::fs;
 use std::path::PathBuf;
 
 fn main() {
-
     //main arrenge
-    let src = "./config/config.json"; 
-    let bat = "src/installer/russel_install.bat";
-    let nssm="src/nssm/nssm.exe";
+    let src = "./config/config.json";
+    let bat = "src/installer/quebrix_install.bat";
+    let nssm = "src/nssm/nssm.exe";
     let out_dir = env::var("OUT_DIR").unwrap();
 
     //windows
-    #[cfg(target_os = "windows")]{
+    #[cfg(target_os = "windows")]
+    {
+        let mut config_dest = PathBuf::from(out_dir.clone());
+        config_dest.pop();
+        config_dest.pop();
+        config_dest.pop();
+        config_dest.push("config");
 
-      let mut config_dest = PathBuf::from(out_dir.clone());
-      config_dest.pop(); 
-      config_dest.pop(); 
-      config_dest.pop(); 
-      config_dest.push("config"); 
+        fs::create_dir_all(&config_dest).unwrap();
 
-      fs::create_dir_all(&config_dest).unwrap();
-  
-      let mut bat_dest = PathBuf::from(out_dir.clone());
-      bat_dest.pop(); 
-      bat_dest.pop(); 
-      bat_dest.pop(); 
-  
-      let mut nssm_dest = PathBuf::from(out_dir.clone());
-      nssm_dest.pop(); 
-      nssm_dest.pop(); 
-      nssm_dest.pop(); 
-      nssm_dest.push("nssm");
+        let mut bat_dest = PathBuf::from(out_dir.clone());
+        bat_dest.pop();
+        bat_dest.pop();
+        bat_dest.pop();
 
-      fs::create_dir_all(&nssm_dest).unwrap();
+        let mut nssm_dest = PathBuf::from(out_dir.clone());
+        nssm_dest.pop();
+        nssm_dest.pop();
+        nssm_dest.pop();
+        nssm_dest.push("nssm");
 
-      nssm_dest.push("nssm.exe");
-      bat_dest.push("russel_install.bat"); 
-      config_dest.push("config.json"); 
+        fs::create_dir_all(&nssm_dest).unwrap();
 
-      fs::copy(bat, &bat_dest).unwrap();
-      fs::copy(nssm, &nssm_dest).unwrap();
-      fs::copy(src, &config_dest).unwrap();
+        nssm_dest.push("nssm.exe");
+        bat_dest.push("quebrix_install.bat");
+        config_dest.push("config.json");
 
-      println!("cargo:rerun-if-changed={}", src);
-      println!("cargo:rerun-if-changed={}", bat);
-      println!("cargo:rerun-if-changed={}", nssm);
+        fs::copy(bat, &bat_dest).unwrap();
+        fs::copy(nssm, &nssm_dest).unwrap();
+        fs::copy(src, &config_dest).unwrap();
+
+        println!("cargo:rerun-if-changed={}", src);
+        println!("cargo:rerun-if-changed={}", bat);
+        println!("cargo:rerun-if-changed={}", nssm);
     }
 
     //not windows
-    #[cfg(not(target_os = "windows"))]{
-
-       let mut config_dest = PathBuf::from(out_dir.clone());
-       config_dest.pop(); 
-       config_dest.pop(); 
-       config_dest.pop(); 
-       config_dest.push("config"); 
-       fs::create_dir_all(&config_dest).unwrap();
-       config_dest.push("config.json"); 
-       fs::copy(src, &config_dest).unwrap();
-       println!("cargo:rerun-if-changed={}", src);
+    #[cfg(not(target_os = "windows"))]
+    {
+        let mut config_dest = PathBuf::from(out_dir.clone());
+        config_dest.pop();
+        config_dest.pop();
+        config_dest.pop();
+        config_dest.push("config");
+        fs::create_dir_all(&config_dest).unwrap();
+        config_dest.push("config.json");
+        fs::copy(src, &config_dest).unwrap();
+        println!("cargo:rerun-if-changed={}", src);
     }
-
 }
-
